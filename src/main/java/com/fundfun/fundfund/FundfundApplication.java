56d750f91e9047a6e2e62987323e632dd1343633
yeooo <ltbel12@kakao.com>
2023-05-13 23:36:07 +0900

    init: add initial project file

---
+++ src/main/java/com/fundfun/fundfund/FundfundApplication.java
@@ -0,0 +1,13 @@
+package com.fundfun.fundfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundfundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundfundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundfun/fundfund/FundfundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundfun.fundfund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundfundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
