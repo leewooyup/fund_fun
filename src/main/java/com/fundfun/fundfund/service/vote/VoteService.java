82970207fedb7e61fea28979659df8216b3a23a0
ybwi0912 <ybwi0912@naver.com>
2023-05-15 14:19:14 +0900

    feat: Vote Service 수정

--- src/main/java/com/fundfun/fundfund/service/vote/VoteService.java
+++ src/main/java/com/fundfun/fundfund/service/vote/VoteService.java
@@ -1,6 +1,19 @@
 package com.fundfun.fundfund.service.vote;
 
+import com.fundfun.fundfund.domain.vote.Vote;
+import com.fundfun.fundfund.repository.vote.VoteRepository;
+import lombok.RequiredArgsConstructor;
 import net.minidev.json.JSONUtil;
+import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class VoteService {
+    private final VoteRepository voteRep;
+    public Vote selectVoteByPostId(UUID postId){
+
+        return null;
+    }
 }
