77651645c54b4c793f4fb73fe9931decd2500e39
hen-ni <haein0758@naver.com>
2023-05-15 15:08:02 +0900

    init: Entity post

--- src/main/java/com/fundfun/fundfund/domain/post/Post.java
+++ src/main/java/com/fundfun/fundfund/domain/post/Post.java
@@ -1,4 +1,4 @@
-package com.fundfun.fundfund.domain.vote;
+package com.fundfun.fundfund.domain.post;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,25 +8,22 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Vote {
+public class Post {
     @Id
     @GeneratedValue
-    private UUID voteId;
     private UUID postId;
-    private LocalDateTime voteStart;
-    private LocalDateTime voteEnd;
+    private String title;
+    private String content;
+    private String post_date;
+    private int like;
+    private String update_date;
+    private String category;
     private String status;
 
-    public void updateStatus() {
-        if (LocalDateTime.now().isAfter(voteEnd)) {
-            this.status = "end";
-        }
-    }
 }
