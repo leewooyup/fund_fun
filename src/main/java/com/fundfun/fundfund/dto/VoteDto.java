ee1f1cd3ccfc0e7a86711800e68f7871ac62779e
hen-ni <haein0758@naver.com>
2023-05-15 14:18:37 +0900

    init: voteDto, return a

--- src/main/java/com/fundfun/fundfund/dto/VoteDto.java
+++ src/main/java/com/fundfun/fundfund/dto/VoteDto.java
@@ -1,9 +1,16 @@
 package com.fundfun.fundfund.dto;
 import lombok.Data;
+import org.w3c.dom.ls.LSOutput;
+
 public class VoteDto {
     private int voteId;
     private int postId;
     private String voteStart;
     private String voteEnd;
     private String status;
+
+    public String toString() {
+        return "a";
     }
+    }
+
