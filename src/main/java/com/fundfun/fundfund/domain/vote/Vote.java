c1797395039ff00e7774a379dc8bb58286ee5b25
hen-ni <haein0758@naver.com>
2023-05-15 17:32:10 +0900

    vote service

--- src/main/java/com/fundfun/fundfund/domain/vote/Vote.java
+++ src/main/java/com/fundfun/fundfund/domain/vote/Vote.java
@@ -1,10 +1,7 @@
 package com.fundfun.fundfund.domain.vote;
 
 import com.fundfun.fundfund.domain.post.Post;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -14,6 +11,7 @@ import java.util.UUID;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Vote {
     @Id
     @GeneratedValue
--- src/main/java/com/fundfun/fundfund/repository/vote/VoteRepository.java
+++ src/main/java/com/fundfun/fundfund/repository/vote/VoteRepository.java
@@ -1,4 +1,9 @@
 package com.fundfun.fundfund.repository.vote;
 
-public interface VoteRepository {
+import com.fundfun.fundfund.domain.vote.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface VoteRepository extends JpaRepository<Vote, UUID> {
 }
