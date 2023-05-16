7c701c73bbfe5b96143c9fbcbb9c94ba90d33ca4
hen-ni <haein0758@naver.com>
2023-05-16 11:44:56 +0900

    queryDSL

--- src/main/java/com/fundfun/fundfund/service/post/PostServiceImpl.java
+++ src/main/java/com/fundfun/fundfund/service/post/PostServiceImpl.java
@@ -1,103 +1,107 @@
 package com.fundfun.fundfund.service.post;
 
 import com.fundfun.fundfund.domain.post.Post;
+import com.fundfun.fundfund.domain.post.QPost;
 import com.fundfun.fundfund.repository.post.PostRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 import com.fundfun.fundfund.modelmapper.ModelMapperConfig;
 
 
-    @Service
-    @RequiredArgsConstructor
-    public class PostServiceImpl implements PostService {
-        private final PostRepository postRep;
-        private final ModelMapper modelMapper;
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+
+    private final JPAQueryFactory query;
+    private final PostRepository postRep;
+    private final ModelMapper modelMapper;
 
 
-        //전체 게시물 조회
-        public List<Post> selectAll() {
-            return postRep.findAll();
+    //전체 게시물 조회
+    public List<Post> selectAll() {
+        return postRep.findAll();
+    }
+
+    //제목으로 게시물 조회
+    public List<Post> selectPostByKeyword(String keyword) {
+        QPost p = QPost.post;
+        //List<Post> list = query.select().from(p).w
+        return null;
+    }
+
+    //작성자로 게시물 조회
+    public List<Post> selectPostByUserId(UUID userId) {
+        return null;
+    }
+
+    //상태로 게시물 조회
+    public List<Post> selectPostByStatus(String status) {
+        return null;
+    }
+
+    fin
+    //카테고리로 게시물 조회
+    public List<Post> selectPostByCategory(String categoryPost) {
+        QPost qpost = QPost.post;
+        return query.selectFrom(qpost)
+                .where(qpost.categoryPost.eq(categoryPost))
+                .fetch();
+    }
+
+    //게시물 생성
+    public void createPost(Post post) {
+        postRep.save(post);
+    }
+
+    //게시물 삭제
+    public void deletePost(Post post) {
+
+        postRep.delete(post);
+
+    }
+
+    public void updatePost(Post post) {
+        Post existingPost = postRep.findById(post.getId()).orElse(null);
+        if (existingPost != null) {
+            //변경 필드값 업데이트
+            existingPost.setTitle(post.getTitle());
+
+            //게시물저장
+            postRep.save(existingPost);
         }
+    }
 
-        //제목으로 게시물 조회
-        public List<Post> selectPostByKeyword(String keyword) {
-            return null;
-        }
 
-        //작성자로 게시물 조회
-        public List<Post> selectPostByUserId(UUID userId) {
-            return null;
-        }
+    //좋아요 순으로 게시물 정렬
+    public List<Post> getPostsOrderByLikes() {
 
-        //상태로 게시물 조회
-        public List<Post> selectPostByStatus(String status) {
-            return null;
-        }
+        Post post = Post.builder()
+                .title("Example Title")
+                .contentPost("Example Content")
+                .build();
 
-        //카테고리로 게시물 조회
-        public List<Post> selectPostByCategory(String category) {
-            return null;
-        }
+        return postRep.findAll(Sort.by(Sort.Direction.DESC, "like"));
 
-        //게시물 생성
-        public void createPost(Post post) {
-
-            postRep.save(post);
-        }
-
-        //게시물 삭제
-        public void deletePost(Post post) {
-
-            postRep.delete(post);
-
-        }
-
-//게시물 수정
-
-        public void updatePost(Post post) {
-
-            Optional<Post> optionalPost = postRep.findById(post.getId());
-            if (optionalPost.isPresent()) {
-                Post existingPost = optionalPost.get();
-
-                // 변경할 필드값을 업데이트합니다.
-                existingPost.setTitle(post.getTitle());
-                existingPost.setContent(post.getContent());
-
-                // 게시물을 저장하여 업데이트합니다.
-                postRep.save(existingPost);
-
-            }
-
-        }
-
-        //좋아요 순으로 게시물 정렬
-        public List<Post> getPostsOrderByLikes() {
-
-            Post post = Post.builder()
-                    .title("Example Title")
-                    .content("Example Content")
-                    .build();
-
-            return postRep.findAll(Sort.by(Sort.Direction.DESC, "like"));
-
-        }
+    }
 
 //좋아요 100 개 이상 시 상태 변경
 
-        public void updatePostStatus(Post post) {
+    public void updatePostStatus(Post post) {
 
-            if (post.getLike() >= 100) {
-                post.setStatus("update preproduct");
+        if (post.getLikePost() >= 100) {
+            post.setStatusPost("update preproduct");
 
-                postRep.save(post);
-
-            }
+            postRep.save(post);
 
         }
+
     }
+}
