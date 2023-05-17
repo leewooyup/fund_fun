78d66675659b3b7883e27c6d31b4bc1585d52152
ybwi0912 <ybwi0912@naver.com>
2023-05-17 11:34:07 +0900

    Fix : Vote

--- src/main/java/com/fundfun/fundfund/service/vote/VoteService.java
+++ src/main/java/com/fundfun/fundfund/service/vote/VoteService.java
@@ -8,17 +8,17 @@ import java.util.UUID;
 public interface VoteService {
 
     //투표 생성
-    public Vote createVote(Vote vote);
+    Vote createVote(Vote vote);
 
     //투표 전체 조회
-    public List<Vote> selectAll();
+    List<Vote> selectAll();
 
     //투표 아이디로 조회
-    public Vote selectVoteById(UUID voteId);
+    Vote selectVoteById(UUID voteId);
 
     //voteId에 해당하는 투표의 상태 업데이트
-    public void updateVoteStatus(UUID voteId);
+    void updateVoteStatus(UUID voteId);
     
     //투표 삭제
-    public void deleteVote(UUID voteId);
+    void deleteVote(UUID voteId);
 }
