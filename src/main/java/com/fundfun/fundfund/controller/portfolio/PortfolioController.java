97d1168ec5f5149773b1642be37388f3498cf75d
hen-ni <90090783+hen-ni@users.noreply.github.com>
2023-05-22 17:58:18 +0900

    Hen221 (#73)
    * fix: form 파일업로드 오류 수정
    
    * Feat: 상품 등록시 업로드한 상품썸네일이 product_list에 표시되도록
    
    * Feat: 썸네일 등록 안한 상품에 대해서는 기본 avatar이미지 노출
    
    * Feat: product update 기능 수정
    
    * Feat: 폼 로그인 구현
    
    * Fix: Builder -> SuperBuilder
    
    * Fix: 세션정보 유지, 권한 값 설정, Feat: UserContext 추가
    
    * Feat: OAuth2UserService 구현완료, 카카오 로그인
    
    * Fix: warning 필드 final or @Builder.Default로 해결
    
    * Feat: selectbox 기능 구현중
    
    * Feat: product update 기능 수정
    
    * Feat: selectbox
    
    * Fix: Role 값 int값으로 변경
    
    * Fix: Infinite recursion, @JsonIgnore로 해결
    
    * Feat: UserContext에 Users 정보추가
    
    * feat: controller(u, w, r)
    
    * fix: portfolio delete
    
    * fix : controller
    
    ---------
    
    Co-authored-by: leewooyup <zjadlspun1114@naver.com>
    Co-authored-by: lee5917 <gdiop74@naver.com>
    Co-authored-by: lee5917 <127364682+lee5917@users.noreply.github.com>

--- src/main/java/com/fundfun/fundfund/controller/portfolio/PortfolioController.java
+++ src/main/java/com/fundfun/fundfund/controller/portfolio/PortfolioController.java
@@ -2,12 +2,14 @@ package com.fundfun.fundfund.controller.portfolio;
 
 import com.fundfun.fundfund.controller.post.PostForm;
 import com.fundfun.fundfund.domain.portfolio.Portfolio;
+import com.fundfun.fundfund.domain.user.Users;
 import com.fundfun.fundfund.dto.portfolio.PortfolioDto;
 import com.fundfun.fundfund.dto.vote.VoteDto;
 import com.fundfun.fundfund.service.portfolio.PortfolioService;
 import com.fundfun.fundfund.service.vote.VoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -33,7 +35,7 @@ public class PortfolioController {
     public String goPortfolioList(HttpServletRequest req, HttpServletResponse res, Model model) {
         String reqData = req.getParameter("postId");
 
-        if(reqData == null) {
+        if (reqData == null) {
             model.addAttribute("state", "noId");
         } else {
             model.addAttribute("postId", UUID.fromString(reqData));
@@ -50,14 +52,13 @@ public class PortfolioController {
      * - R : 상세보기
      * - D : 삭제
      */
-    
-    //페이지 상세보기
+
     @GetMapping("/portfolio/goDetail")
     public String goDetail(HttpServletRequest req, HttpServletResponse res, Model model) {
         String portfolioId = req.getParameter("id");  //포폴아이디
         String postId = req.getParameter("postId");   //포스트아이디
 
-        if(portfolioId != null) {
+        if (portfolioId != null) {
             UUID id = UUID.fromString(portfolioId);  //uuid로 가져오기 > String 변경
             model.addAttribute("data", portfolioService.selectById(id));
             // portfolioService에서 아이디로 불러오기
@@ -74,7 +75,7 @@ public class PortfolioController {
 
     }
 
-    // 포트폴리오 목록 불러오기 - ajax
+   /* // 포트폴리오 목록 불러오기 - ajax (0521 코드)
     @PostMapping(value = "/portfolio/getData")
     @ResponseBody
     public HashMap<String, Object> getData(@RequestBody Map<String, Object> paramMap) {
@@ -84,9 +85,25 @@ public class PortfolioController {
         map.put("portfolioList", portfolioService.selectPortByPostId(postId));
 
         return map;
+    }*/
+
+    //0522 수정한 부분
+    @PostMapping(value = "/portfolio/getData")
+    @ResponseBody
+    public HashMap<String, Object> getData(@AuthenticationPrincipal Users user,
+                                           @RequestBody Map<String, Object> paramMap) {
+
+        // user 객체를 사용하여 필요한 작업 수행
+        // String userId = String.valueOf(user.getId());
+        HashMap<String, Object> map = new HashMap<>();
+        UUID postId = UUID.fromString(paramMap.get("postId").toString());
+        map.put("portfolioList", portfolioService.selectPortByPostId(postId));
+
+        return map;
     }
 
-    // 포트폴리오 수정/작성 - ajax
+    //0521 코드
+   /* // 포트폴리오 수정/작성 - ajax
     @PostMapping(value = "/portfolio/commitData")
     @ResponseBody
     public HashMap<String, Object> commitData(@RequestBody Map<String, Object> paramMap) {
@@ -109,9 +126,7 @@ public class PortfolioController {
                 UUID portfolioId = UUID.fromString(paramMap.get("portfolioId").toString());
                 portfolioDto.setId(portfolioId);
                 portfolioService.deletePort(portfolioDto);
-
             } else { //포트폴리오 수정 및 생성
-
                 portfolioDto.setTitle(paramMap.get("title").toString());
                 portfolioDto.setBeneRatio(Float.parseFloat(paramMap.get("beneRatio").toString()));
                 portfolioDto.setWarnLevel(paramMap.get("warnLevel").toString());
@@ -131,5 +146,58 @@ public class PortfolioController {
         } finally {
             return map;
         }
+    }*/
+
+    //0522 코드
+    // 포트폴리오 수정/작성 - ajax
+    @PostMapping(value = "/portfolio/commitData")
+    @ResponseBody
+    public HashMap<String, Object> commitData(@AuthenticationPrincipal Users user,
+                                              @RequestBody Map<String, Object> paramMap) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("msg", "fail");
+
+        //펀드매니저만 설정되게 해놓고 싶음.
+        if (user.getRole().equals("fund_manager")) {
+            try {
+                PortfolioDto portfolioDto = new PortfolioDto();
+                String state = paramMap.get("state").toString();
+
+                UUID postId = UUID.fromString(paramMap.get("postId").toString());
+                VoteDto voteDto = voteService.selectVoteByPostId(postId);
+
+                portfolioDto.setPostId(postId);
+                portfolioDto.setVoteId(voteDto.getId());
+
+                // 포트폴리오 삭제
+                if (state.equals("D")) {
+                    UUID portfolioId = UUID.fromString(paramMap.get("portfolioId").toString());
+                    portfolioDto.setId(portfolioId);
+                    portfolioService.deletePort(portfolioDto);
+                } else { //포트폴리오 수정 및 생성
+                    portfolioDto.setTitle(paramMap.get("title").toString());
+                    portfolioDto.setBeneRatio(Float.parseFloat(paramMap.get("beneRatio").toString()));
+                    portfolioDto.setWarnLevel(paramMap.get("warnLevel").toString());
+                    portfolioDto.setContentPortfolio(paramMap.get("content").toString());
+
+                    if (state.equals("W")) {
+                        portfolioService.createPort(portfolioDto);
+                    } else {
+                        UUID portfolioId = UUID.fromString(paramMap.get("portfolioId").toString());
+                        portfolioDto.setId(portfolioId);
+                        portfolioService.updatePort(portfolioDto);
+                    }
+                }
+                map.put("msg", "success");   //성공
+            } catch (Exception e) {
+                System.out.println("Portfolio 수정/등록 시 에러 : " + e);
+            } finally {
+                return map;
+            }
+        } else {
+            //펀드매니저만 가능함 등록되지 않음;
+        }
+        return map;
     }
 }
+
--- src/main/java/com/fundfun/fundfund/domain/post/Post.java
+++ src/main/java/com/fundfun/fundfund/domain/post/Post.java
@@ -1,8 +1,6 @@
 package com.fundfun.fundfund.domain.post;
 
 
-import com.fundfun.fundfund.domain.reply.Reply;
-import com.fundfun.fundfund.domain.user.Users;
 import com.fundfun.fundfund.domain.vote.Vote;
 import com.fundfun.fundfund.dto.vote.VoteDto;
 import lombok.AllArgsConstructor;
@@ -14,7 +12,6 @@ import com.fundfun.fundfund.domain.portfolio.Portfolio;
 import com.fundfun.fundfund.util.BaseTimeEntity;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-import net.minidev.json.annotate.JsonIgnore;
 import org.hibernate.annotations.ColumnDefault;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -46,25 +43,14 @@ public class Post extends BaseTimeEntity {
     private int likePost = 0;
     @Builder.Default
     private String categoryPost = "주식형";
-    @OneToOne(cascade = CascadeType.REMOVE)
+    @OneToOne
     private Vote vote;
     //@ColumnDefault("'EARLY_IDEA'")
     @Builder.Default
     private StPost statusPost = StPost.EARLY_IDEA;
-
-    @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name="user_id")
-    private Users user;
-
-//    @OneToMany(mappedBy = "post")
-//    private List<Portfolio> portfolios = new ArrayList<>();
-
-    @OneToMany(mappedBy="post")
-    private List<Reply> replies = new ArrayList<>();
-
-
-
+    @OneToMany(mappedBy = "post")
+    @Builder.Default
+    private List<Portfolio> portfolios = new ArrayList<>();
     public void setStatusPost(StPost statusPost) {
         this.statusPost = statusPost;
     }
