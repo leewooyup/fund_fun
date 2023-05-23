7d82057447d39dd3f109810bebfded3df3b73c01
yeoooo <ruisya330@gmail.com>
2023-05-23 10:31:05 +0900

    feat: 알람 기능 추가

--- src/main/java/com/fundfun/fundfund/domain/alarm/AlarmDTO.java
+++ src/main/java/com/fundfun/fundfund/domain/alarm/AlarmDTO.java
@@ -3,12 +3,13 @@ package com.fundfun.fundfund.domain.alarm;
 import com.fundfun.fundfund.domain.user.Users;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class AlarmDTO {
     private Long id;
-    private Users user;
     private String content;
     private boolean is_read;
 
