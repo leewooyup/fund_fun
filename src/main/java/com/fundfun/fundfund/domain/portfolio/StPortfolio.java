7cea42976e73ab7d4a4885e2d4f462b8b4c10fea
ybwi0912 <ybwi0912@naver.com>
2023-05-26 11:19:22 +0900

    Feat: Vote, Portfolio 투표 종료 후 승자 선출 로직

---
+++ src/main/java/com/fundfun/fundfund/domain/portfolio/StPortfolio.java
@@ -0,0 +1,12 @@
+package com.fundfun.fundfund.domain.portfolio;
+
+public enum StPortfolio {
+    CANDIDATE(0),
+    WINNER(1);
+
+    private int value;
+
+    StPortfolio(int i){
+        this.value = value;
+    }
+}
