681bdd6d98c788e0f17de2a37c9ad4bfc9a4ca21
yeoooo <ruisya330@gmail.com>
2023-05-26 14:27:29 +0900

    feat: mapping for home index

--- src/main/java/com/fundfun/fundfund/controller/HomeController.java
+++ src/main/java/com/fundfun/fundfund/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.fundfun.fundfund.controller;
 
+import com.fundfun.fundfund.domain.post.Post;
+import com.fundfun.fundfund.domain.user.Role;
 import com.fundfun.fundfund.domain.user.UserAdapter;
 import com.fundfun.fundfund.domain.user.UserDTO;
+import com.fundfun.fundfund.service.post.PostService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,9 +17,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class HomeController {
     private final ModelMapper modelMapper;
+    private final PostService postService;
+
     @GetMapping("/")
     public String index(@AuthenticationPrincipal UserAdapter adapter, Model model) {
 
+        if (adapter == null) {
+            return "index";
+        }else{
+            System.out.println("adapter = " + adapter.getUser().getRole());
+            if (adapter.getUser().getRole() == Role.COMMON) {
+                return "index_user";
+            } else if (adapter.getUser().getRole() == Role.FUND_MANAGER) {
+                return "index_fund";
+            }
+        }
+
         return "index";
+
     }
 }
