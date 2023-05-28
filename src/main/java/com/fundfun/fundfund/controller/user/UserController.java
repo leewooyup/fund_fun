cc749a7f72c7307b802c25219d1370d5ec541fa9
yeoooo <ruisya330@gmail.com>
2023-05-28 20:41:42 +0900

    feat: 프로필 이미지 추가중
    -서버가 가동중에 만들어진 이미지에 대해서 요청을 할 수 없는 404 에러 발생
    -서버를 재 기동하면 이전에 만들었던 이미지를 인식
    -시연을 위한 데이터 추가

--- src/main/java/com/fundfun/fundfund/controller/user/UserController.java
+++ src/main/java/com/fundfun/fundfund/controller/user/UserController.java
@@ -4,18 +4,30 @@ import com.fundfun.fundfund.domain.user.*;
 import com.fundfun.fundfund.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import org.springframework.util.FileCopyUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Objects;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -42,7 +54,6 @@ public class UserController {
 
     @PostMapping("/register")
     public String register(RegisterForm form, @RequestParam("role") String role) {
-
         UserDTO user = userService.register(Users.builder()
                 .count(0L)
                 .email(form.getEmail())
@@ -78,20 +89,88 @@ public class UserController {
     }
 
     @GetMapping("/mypage/edit")
-    public String myPageForm(@AuthenticationPrincipal UserAdapter adapter, Model model) {
+    public String myPageForm(@AuthenticationPrincipal UserAdapter adapter, Model model) throws IOException {
+        String directoryPath = "src/main/resources/static/img/profiles";
+        File directory = new File(directoryPath);
+        if (!directory.exists()) {
+            throw new IllegalArgumentException("디렉토리를 찾을 수 없습니다: " + directoryPath);
+        }
+
+        File[] files = directory.listFiles();
+        System.out.println("files.toString() = " + Arrays.toString(files));
+        String fileName = "default.jpeg";
+        if (files == null) {
+            throw new IOException("디렉토리 내 파일을 읽어올 수 없습니다: " + directoryPath);
+        }
+        String fileNamePattern = adapter.getUser().getId().toString().replace("-","");
+
+        for (File file : files) {
+            if (file.isFile() && file.getName().split("\\.")[0].equals(fileNamePattern)) {
+                fileName = file.getName();
+                break;
+            }
+        }
+        System.out.println("adapter = " + adapter.getUser().getId());
+        model.addAttribute("imgDir", fileName.replace("-",""));
         model.addAttribute("form", new UserUpdateForm());
         return "user/mypage_form";
     }
 
     @PostMapping("/mypage/edit")
-    public String editMyPage(@AuthenticationPrincipal UserAdapter adapter, @Valid UserUpdateForm form, BindingResult result) {
+    @Transactional
+    public String editMyPage(@AuthenticationPrincipal UserAdapter adapter, @Valid UserUpdateForm form) {
+        String directoryPath = "src/main/resources/static/img/profiles";
+        File directory = new File(directoryPath);
+        if (!directory.exists() || !directory.isDirectory()) {
+            // 디렉토리가 존재하지 않거나 디렉토리가 아닌 경우 처리 로직 추가
+            // 예를 들어, 디렉토리를 생성하거나 오류를 반환할 수 있습니다.
+            System.out.println("directory.exists() = " + directory.exists());
+            System.out.println("directory = " + directory.isDirectory());
+        }
+        File[] files = directory.listFiles();
+//        System.out.println(new File("/main/resources/static/img/profiles/").listFiles().toString());
+        String fileName = "";
+
+        String fileNamePattern = adapter.getUser().getId().toString();
+        File targetFile = null;
+        System.out.println("files = " + files);
+
+        for (File file : files) {
+            if (file.isFile() && file.getName().matches(fileNamePattern)) {
+                fileName = file.getName();
+                targetFile = file;
+                break;
+            }
+        }
+
+        if (targetFile != null) {
+            targetFile.delete();
+        }
+
+
+
+        String base64Data = form.getImage().split(",")[1]; // 데이터 URL에서 실제 데이터 부분 추출
+        String type = form.getImage().split(",")[0].split("/")[1].split(";")[0];
+        byte[] decodedBytes = Base64.getDecoder().decode(base64Data); // Base64 디코딩
+
+
+        File file = new File("src/main/resources/static/img/profiles/" + adapter.getUser().getId().toString().replace("-","") + "." + type);
+        System.out.println("경로 = " + file.getPath());
+
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            FileCopyUtils.copy(decodedBytes, fos); // 파일로 저장
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         UserDTO dto = userService.findById(adapter.getUser().getId());
         userService.update(dto.getId(), UserDTO
                 .builder()
                 .id(dto.getId())
                 .password(dto.getPassword())
                 .name(dto.getName())
-                .image(form.getImage() == null ? dto.getImage() : form.getImage())
                 .email(form.getEmail() == null ? dto.getEmail() : form.getEmail())
                 .phone(form.getPhone() == null ? dto.getPhone() : form.getPhone())
                 .role(dto.getRole())
